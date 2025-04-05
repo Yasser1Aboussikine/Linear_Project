e07e66242f790d0b02b66354c5e0693c8148b22c
Ikram Ghala <ikram.ghala120@gmail.com>
2025-04-05 23:07:00 +0000

    routing

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,22 +1,19 @@
-import './App.css'
-import {Link, Router, Routes, Route} from "react-router-dom"
+import "./App.css";
+import { Router, Routes, Route } from "react-router-dom";
 
 // celebrity look Alike
 // emotion detection
 
 function App() {
-
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/home" element={<div>HOOOME</div>}/>
-          <Route path="/recognize" element={<div>REcogniiiez</div>} />
-          <Route path='/about' element={<div>ABOOOOUTTT</div>} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/home" element={<div>HOOOME</div>} />
+        <Route path="/recognize" element={<div>REcogniiiez</div>} />
+        <Route path="/about" element={<div>ABOOOOUTTT</div>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
